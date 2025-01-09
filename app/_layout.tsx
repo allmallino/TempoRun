50518ea5ee2a03f20a2eb2de9d83f60dfd1f0d8e
allmall <zavarzin03@gmail.com>
2025-01-09 12:23:22 +0200

    feat: added Roboto font and set it to be by default

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,7 +17,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
   });
 
   useEffect(() => {
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -15,7 +15,7 @@ export function ThemedText({
   return (
     <Text
       style={[
-        { color },
+        { color, fontFamily: "Roboto" },
         type === "default" ? styles.default : undefined,
         type === "title" ? styles.title : undefined,
         type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
